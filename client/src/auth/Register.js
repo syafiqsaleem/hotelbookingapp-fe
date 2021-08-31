fdea40c5d5c020a07826d981aec8e13ba4ab7e72
syafiqsaleem <19syafiq89@gmail.com>
2021-08-31 20:51:55 +0800

    Register Page in-progress

--- client/src/auth/Register.js
+++ client/src/auth/Register.js
@@ -1,6 +1,35 @@
+import { useState } from "react";
+
 const Register = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  // Upon clicking submit button, data will be sent to backend
+  const handleSubmit = () => {
+    alert("send user info to backend");
+  };
+
+  // Register details
+  const registerForm = () => (
+    <form onSubmit={handleSubmit}>
+      show the form <button type="submit">Submit</button>
+      {""}
+    </form>
+  );
+
   return (
-    <div className="container-fluid h1 p-5 text-center">Register Page</div>
+    <>
+      <div className="container-fluid bg-secondary p-5 text-center">
+        <h1>Register</h1>
+      </div>
+
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6 offset-md-3">{registerForm()}</div>
+        </div>
+      </div>
+    </>
   );
 };
 
