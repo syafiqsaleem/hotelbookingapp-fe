75b3aa904afcb59f1ba328232dbbe9a98965ec9c
syafiqsaleem <19syafiq89@gmail.com>
2021-09-02 14:22:47 +0800

    Setting up Axios

--- client/src/auth/Register.js
+++ client/src/auth/Register.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import RegisterForm from "../components/RegisterForm";
+import axios from "axios";
 
 const Register = () => {
   const [name, setName] = useState("");
@@ -7,9 +8,19 @@ const Register = () => {
   const [password, setPassword] = useState("");
 
   // Upon clicking submit button, data will be sent to backend
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.table({ name, email, password });
+    // console.table({ name, email, password });
+    try {
+      const res = await axios.post(`http://localhost:8000/api/register`, {
+        name,
+        email,
+        password,
+      });
+      console.log("REGISTER USER ===>", res);
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   return (
