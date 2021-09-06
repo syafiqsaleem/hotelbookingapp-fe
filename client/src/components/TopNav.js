afb133fc01ca8eb68018b0c6749aaeebbb257a2d
syafiqsaleem <19syafiq89@gmail.com>
2021-09-06 21:58:37 +0800

    conditional rendring and logout

--- client/src/components/TopNav.js
+++ client/src/components/TopNav.js
@@ -1,19 +1,47 @@
 import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
 
 // Navigation Menu
-const TopNav = () => (
-  <div className="nav bg-light d-flex justfy-content-between">
-    {/* No page reload because we use Link component */}
-    <Link className="nav-link" to="/">
-      Home
-    </Link>
-    <Link className="nav-link" to="/login">
-      Login
-    </Link>
-    <Link className="nav-link" to="/register">
-      Register
-    </Link>
-  </div>
-);
+const TopNav = () => {
+  const dispatch = useDispatch();
+  const { auth } = useSelector((state) => ({ ...state }));
+  const history = useHistory();
+
+  const logout = () => {
+    dispatch({
+      type: "LOGOUT",
+      payload: null,
+    });
+    window.localStorage.removeItem("auth");
+    history.push("/login");
+  };
+
+  return (
+    <div className="nav bg-light d-flex justfy-content-between">
+      {/* No page reload because we use Link component */}
+      <Link className="nav-link" to="/">
+        Home
+      </Link>
+
+      {auth !== null && (
+        <a className="nav-link pointer" onClick={logout}>
+          Logout
+        </a>
+      )}
+
+      {auth === null && (
+        <>
+          <Link className="nav-link" to="/login">
+            Login
+          </Link>
+          <Link className="nav-link" to="/register">
+            Register
+          </Link>
+        </>
+      )}
+    </div>
+  );
+};
 
 export default TopNav;
