30dc1af298d6522792cb8ca07bca1f7eb091fec8
syafiqsaleem <19syafiq89@gmail.com>
2021-09-07 23:19:48 +0800

    Conditional Rendering

--- client/src/hotels/NewHotel.js
+++ client/src/hotels/NewHotel.js
@@ -1,6 +1,4 @@
 const NewHotel = () => {
-  // // destructure state => { user }
-  // const { auth } = useSelector((state) => ({ ...state }));
   return (
     <div className="container-fluid h1 p-5 text-center">Post a new hotel</div>
   );
