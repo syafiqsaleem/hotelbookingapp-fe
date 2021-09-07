e00e3dd3ed7d36c054469303ea37d85325d44283
syafiqsaleem <19syafiq89@gmail.com>
2021-09-07 21:32:43 +0800

    Dashboard Navigation Links

--- client/src/user/Dashboard.js
+++ client/src/user/Dashboard.js
@@ -1,8 +1,14 @@
+import DashboardNav from "../components/DashboardNav";
+
 const Dashboard = () => {
   return (
     <>
       <div className="container-fluid bg-secondary p-5">
         <h1>Dashboard</h1>
+      </div>
+
+      <div className="container-fluid p-4">
+        <DashboardNav />
       </div>
 
       <div className="container">
