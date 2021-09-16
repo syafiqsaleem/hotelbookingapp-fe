6bb5f1bbdb938ad1dac17d097490b21c7f68028b
syafiqsaleem <19syafiq89@gmail.com>
2021-09-16 11:44:12 +0800

    form to add new hotels in-progress

--- client/src/hotels/NewHotel.js
+++ client/src/hotels/NewHotel.js
@@ -1,6 +1,67 @@
+import { useState } from "react";
+import { toast } from "react-toastify";
+import AlgoliaPlaces from "algolia-places-react";
+
 const NewHotel = () => {
+  // state
+  const [values, setValues] = useState({
+    title: "",
+    content: "",
+    location: "",
+    image: "",
+    price: "",
+    from: "",
+    to: "",
+    bed: "",
+  });
+
+  //destructuring variables from state
+  const { title, content, location, image, price, from, to, bed } = values;
+
+  const handleSubmit = (e) => {
+    //
+  };
+
+  const handleImageChange = (e) => {
+    //
+  };
+
+  const handleChange = (e) => {
+    //
+  };
+
+  const hotelForm = () => (
+    <form onSubmit={handleSubmit}>
+      <div className="form-group">
+        <label className="btn btn-outline-secondary btn-block m-2 text-left">
+          Image
+          <input
+            type="file"
+            name="image"
+            onChange={handleImageChange}
+            accept="image/*"
+            hidden
+          />
+        </label>
+      </div>
+    </form>
+  );
+
   return (
-    <div className="container-fluid h1 p-5 text-center">Post a new hotel</div>
+    <>
+      <div className="container-fluid bg-secondary p-5 text-center">
+        <h2>Add Hotel</h2>
+      </div>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-10">
+            <br />
+            {hotelForm()}
+          </div>
+          <div className="col-md-2">Image</div>
+        </div>
+      </div>
+    </>
   );
 };
 
