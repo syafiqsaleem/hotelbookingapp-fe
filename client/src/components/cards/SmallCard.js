dd60bb38e983655b082a5ad8f6c5d048c41f82c8
syafiqsaleem <19syafiq89@gmail.com>
2021-09-18 01:17:46 +0800

    conditional rendering between owner & user

--- client/src/components/cards/SmallCard.js
+++ client/src/components/cards/SmallCard.js
@@ -3,7 +3,12 @@ import { diffDays } from "../../actions/hotel";
 import { useHistory, Link } from "react-router-dom";
 import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
 
-const SmallCard = ({ h, handleHotelDelete = (f) => f }) => {
+const SmallCard = ({
+  h,
+  handleHotelDelete = (f) => f,
+  owner = false,
+  showViewMoreButton = true,
+}) => {
   const history = useHistory();
   return (
     <>
@@ -49,19 +54,25 @@ const SmallCard = ({ h, handleHotelDelete = (f) => f }) => {
               </p>
 
               <div className="d-flex justify-content-between h4">
-                <button
-                  onClick={() => history.push(`/hotel/${h._id}`)}
-                  className="btn btn-primary"
-                >
-                  Show more
-                </button>
-                <Link to={`/hotel/edit/${h._id}`}>
-                  <EditOutlined className="text-warning" />
-                </Link>
-                <DeleteOutlined
-                  onClick={() => handleHotelDelete(h._id)}
-                  className="text-danger"
-                />
+                {showViewMoreButton && (
+                  <button
+                    onClick={() => history.push(`/hotel/${h._id}`)}
+                    className="btn btn-primary"
+                  >
+                    Show more
+                  </button>
+                )}
+                {owner && (
+                  <>
+                    <Link to={`/hotel/edit/${h._id}`}>
+                      <EditOutlined className="text-warning" />
+                    </Link>
+                    <DeleteOutlined
+                      onClick={() => handleHotelDelete(h._id)}
+                      className="text-danger"
+                    />
+                  </>
+                )}
               </div>
             </div>
           </div>
