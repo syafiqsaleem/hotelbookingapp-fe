b2dbfc162948513c9d1d6be3e8758758bac60c89
syafiqsaleem <19syafiq89@gmail.com>
2021-09-21 22:34:48 +0800

    search by date and bed

--- client/src/components/forms/Search.js
+++ client/src/components/forms/Search.js
@@ -39,6 +39,31 @@ const Search = () => {
           style={{ height: "50px" }}
         />
       </div>
+
+      <RangePicker
+        onChange={(value, dateString) => setDate(dateString)}
+        disabledDate={(current) =>
+          current && current.valueOf() < moment().subtract(1, "days")
+        }
+        className="w-100"
+      />
+
+      <Select
+        onChange={(value) => setBed(value)}
+        className="w-100"
+        size="large"
+        placeholder="Number of beds"
+      >
+        <Option key={1}>{1}</Option>
+        <Option key={2}>{2}</Option>
+        <Option key={3}>{3}</Option>
+        <Option key={4}>{4}</Option>
+      </Select>
+
+      <SearchOutlined
+        onClick={handleSubmit}
+        className="btn btn-primary p-3 btn-square"
+      />
     </div>
   );
 };
